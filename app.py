4bc6350861fdf91ef659a47d66f9385753b16deb
jyahn <jyahn@example.com>
2023-07-17 12:28:06 +0900

    post, get step

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET, POST'])
 def index():
-    print(dict(request.headers))
-    return render_template('index.html')
+    if request.method == "GET":
+        os_info = dict(request.headers)
+        print(os_info)
+        return render_template('index.html', header=os_info)
+    elif request.method == "POST":
+        return render_template('index.html', header="점심시간이에요")
 
 if __name__ == '__main__':
     app.run(debug = True)
